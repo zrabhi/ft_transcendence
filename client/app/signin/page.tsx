038a16b92a5b5e9120bece3e177284d2ec2d89e5
Mohamed KARIM <karimkar999@gmail.com>
2023-08-02 11:14:25 +0100

    add autofocus to email in sign in form

--- client/app/signin/page.tsx
+++ client/app/signin/page.tsx
@@ -7,45 +7,47 @@ import pongGif from './../../public/pong.gif'
 
 export default function SignIn () {
   return (
-    <div className='container'>
+    <>
       <Navbar />
-      <div className="singin-content">
-        <div className="img">
-          <Image
-            src={pongGif}
-            width={400}
-            height={400}
-            alt="video showing the pong goame who looks like"
-          />
-        </div>
-        <div className="singin">
-          <h2>Join the game!</h2>
-          <p>Go inside the best gamers social network!</p>
-          <div className="auth">
-            <form action="">
-              <label htmlFor="">email</label>
-              <input type="email" placeholder='email' />
-              <label htmlFor="">password</label>
-              <div className="forgot-pass">
-                <Link href="#">Forgot your password?</Link>
-              </div>
-              <input type='password' placeholder='password' ></input>
-              <div className="pass-strength">
-                <div className="bar">
-                  <div className="green-bar">
-
-                  </div>
+      <div className='container'>
+        <div className="singin-content">
+          <div className="img">
+            <Image
+              src={pongGif}
+              width={400}
+              height={400}
+              alt="video showing the pong goame who looks like"
+            />
+          </div>
+          <div className="singin">
+            <h2>Join the game!</h2>
+            <p>Go inside the best gamers social network!</p>
+            <div className="auth">
+              <form action="">
+                <label htmlFor="">email</label>
+                <input type="email" placeholder='email' autoFocus/>
+                <label htmlFor="">password</label>
+                <input type='password' placeholder='password' ></input>
+                <div className="forgot-pass">
+                  <Link href="#">Forgot your password?</Link>
                 </div>
-                <div className="text">week password</div>
+                <div className="pass-strength">
+                  <div className="bar">
+                    <div className="green-bar">
+
+                    </div>
+                  </div>
+                  <div className="text">week password</div>
+                </div>
+              </form>
+              <div className="checkbox">
+                <input type="checkbox" name="terms" id="terms" value="terms" />
+                <label htmlFor="">I agree to <Link href="#" >terms & conditions</Link> </label>
               </div>
-            </form>
-            <div className="checkbox">
-              <input type="checkbox" name="terms" id="terms" value="terms" />
-              <label htmlFor="">I agree to <Link href="#" >terms & conditions</Link> </label>
             </div>
           </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }

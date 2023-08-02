7ef70bd4192f219c302002633eddeb494704363f
Mohamed KARIM <karimkar999@gmail.com>
2023-08-02 13:06:16 +0100

    Fix footer issue on pages who have less than 100 vh

--- client/components/Footer.tsx
+++ client/components/Footer.tsx
@@ -4,14 +4,12 @@ import './styles/Footer.scss'
 export default function Footer() {
   return (
     <div className="footer">
-      <div className="container">
-        <p>
-          Made with Love by <Link href="https://github.com/skeet1" target='_blank'>Skeet</Link> 
-          <Link href="https://github.com/Mohamed-Moumni" target='_blank'> Rigor</Link>
-          <Link href="https://github.com/zrabhi" target='_blank'> zack</Link>
-          <Link href="https://github.com/youssef-badaoui" target='_blank'> YOS3F</Link>
-        </p>
-      </div>
+      <p>
+        Made with Love by <Link href="https://github.com/skeet1" target='_blank'>Skeet</Link> 
+        <Link href="https://github.com/Mohamed-Moumni" target='_blank'> Rigor</Link>
+        <Link href="https://github.com/zrabhi" target='_blank'> zack</Link>
+        <Link href="https://github.com/youssef-badaoui" target='_blank'> YOS3F</Link>
+      </p>
     </div>
   )
 }

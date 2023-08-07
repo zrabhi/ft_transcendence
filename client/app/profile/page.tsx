22157f7fcfbb53f869df406a3f32bf591d868e91
Mohamed KARIM <karimkar999@gmail.com>
2023-08-07 19:11:43 +0100

    Done with profile_card

--- client/app/profile/page.tsx
+++ client/app/profile/page.tsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './style.scss'
 import SideBar from '@/components/SideBar/SideBar'
+import ProfileCard from '@/components/Profile/ProfileCard/ProfileCard'
 
 export default function Profile() {
   return (
     <div className="profile-page text-white">
       <SideBar />
       <div className="profile">
-        profile
+        <div className="profile-content">
+          <ProfileCard />
+        </div>
       </div>
     </div>
   )

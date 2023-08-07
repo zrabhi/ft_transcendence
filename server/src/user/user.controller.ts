5daa02f145e9003964b215bf3a21763f99f7f9b3
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-08-07 12:29:59 +0100

    create basic routes for user entity

--- server/src/user/user.controller.ts
+++ server/src/user/user.controller.ts
@@ -1,4 +1,34 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { Achievement, User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
 
-@Controller('user')
-export class UserController {}
+@Controller('/api/users')
+export class UserController {
+    constructor(private userService:UserService) {}
+
+    @Get('/')
+    async getAllUsers():Promise<User[]>{
+        return await this.userService.findAllUsers();
+    }
+    
+    @Get('/:user_id')
+    async getUser(@Param('user_id') user_id:string):Promise<User>{
+        return await this.userService.findUserById(user_id);
+    }
+
+    @Post('/')
+    async createUser(@Body() createUserDto:CreateUserDto){
+        return await this.userService.addUser(createUserDto);
+    }
+
+    @Delete('/:username')
+    async deleteUser(@Param('username') username:string){
+        return await this.userService.deleteUserByUsername(username);
+    }
+
+    @Get('/:user_id/achievement')
+    async getUserAchievement(@Param('user_id') user_id:string):Promise<Achievement>{
+        return await this.userService.achievementById(user_id);
+    }
+}
--- server/src/user/user.service.ts
+++ server/src/user/user.service.ts
@@ -1,4 +1,98 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
+import { PrismaService } from '../prisma/prisma.service';
+import { Achievement, User } from '@prisma/client';
+import { CreateUserDto } from './dto/create-user.dto';
+import * as bcrypt from 'bcrypt';
+import { triggerAsyncId } from 'async_hooks';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+    constructor(private prismaService:PrismaService) {}
+
+    async findAllUsers():Promise<User[]>{
+        return await this.prismaService.user.findMany({});
+    }
+    
+    async findUserById(user_id:string):Promise<User>{
+        try
+        {
+            return await this.prismaService.user.findUnique({
+                where:
+                {
+                    id:user_id,
+                },
+            })
+        } catch (error) {
+            throw new HttpException({
+                status: HttpStatus.NOT_FOUND,
+                error: `This User_id:${user_id} is not found.`,
+            }, HttpStatus.NOT_FOUND)
+        }
+    }
+
+    async addUser(createUserDto:CreateUserDto){
+        let exist = !!await this.prismaService.user.findFirst({
+            where: {
+                username: createUserDto.username,
+            }
+        })
+        if (exist)
+        {
+            throw new HttpException({
+                status: HttpStatus.FORBIDDEN,
+                error: `This Username:${createUserDto.username} already used`,
+            }, HttpStatus.FORBIDDEN, {
+            })
+        }
+        createUserDto.password = await  bcrypt.hash(createUserDto.password, 10);
+        try {
+            return await this.prismaService.user.create({
+                data:
+                {
+                    email:createUserDto.email,
+                    username:createUserDto.username,
+                    password:createUserDto.password,
+                    achievement:{
+                        create:{
+                            accountCreationAchie:true,
+                        }
+                    }
+                },
+                select: {
+                    id: true,
+                }
+            });
+        } catch(error) {
+
+        }
+    }
+
+    async deleteUserByUsername(username:string){
+        try {
+            return await this.prismaService.user.delete({
+                where:{
+                    id:username,
+                },
+                select:{id:true}
+            });
+        } catch(error) {
+            throw new HttpException({
+                status: HttpStatus.NO_CONTENT,
+                error: `There is no content for ${username}`,
+            }, HttpStatus.NO_CONTENT, {
+            })
+        }
+    }
+
+    async achievementById(user_id:string):Promise<Achievement>{
+        try {
+            return await this.prismaService.achievement.findUnique({
+                where:{
+                    userId: user_id,
+                }
+            })
+        } catch(error) {
+
+        }
+    }
+}

88279fab5d633fad953329239da8fcbf0e576db9
Mohamed-Moumni <mmoumni@student.1337.ma>
2023-08-15 16:53:19 +0100

    change port of the app to 8080

--- server/dist/main.js
+++ server/dist/main.js
@@ -6,7 +6,7 @@ const common_1 = require("@nestjs/common");
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.useGlobalPipes(new common_1.ValidationPipe({ stopAtFirstError: true }));
-    await app.listen(3000);
+    await app.listen(8080);
 }
 bootstrap();
 //# sourceMappingURL=main.js.map

728a6ed2a8494e9431f80732b086e63c4937a030
Mohamed KARIM <karimkar999@gmail.com>
2023-08-24 17:07:35 +0100

    add the navbar manipulator

---
+++ client/app/(navbar)/game-history/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ContainerBox from '@/components/MainPage/ContainerBox/ContainerBox'
+
+export default function GameHistory() {
+  return (
+    <div className="game-history text-white">
+      <ContainerBox component="GameHistory" />
+    </div>
+  )
+}

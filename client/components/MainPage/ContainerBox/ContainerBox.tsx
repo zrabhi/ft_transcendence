8bbcfd17a9e49b3297cfa73fd66608a97e577766
Mohamed KARIM <karimkar999@gmail.com>
2023-08-25 09:26:00 +0100

    Done with instructions page

--- client/components/MainPage/ContainerBox/ContainerBox.tsx
+++ client/components/MainPage/ContainerBox/ContainerBox.tsx
@@ -2,17 +2,18 @@ import React from 'react'
 import Navbar from '@/components/MainPage/NavBar/Navbar'
 import MainContent from '@/components/MainPage/MainContent/MainContent'
 import GameHistory from '@/components/MainPage/GameHistory/GameHistory'
+import Instructions from '@/components/MainPage/Instructions/Instructions'
 import './ContainerBox.scss'
 
 export default function ContainerBox({component}: any) {
   return (
     <div className="container-box">
-          <Navbar />
-          <div className="grow bottom-of-nav flex justify-center items-center ">
-            {component === "MainContent" && <MainContent />}
-            {component === "GameHistory" && <GameHistory />}
-          </div>
+      <Navbar />
+      <div className="grow bottom-of-nav flex justify-center items-center ">
+        {component === "MainContent" && <MainContent />}
+        {component === "GameHistory" && <GameHistory />}
+        {component === "instructions" && <Instructions />}
+      </div>
     </div>
   )
 }
-

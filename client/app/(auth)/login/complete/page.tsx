d98ca11e4b9296b03584626b7081c9dfcec790a7
Mohamed KARIM <karimkar999@gmail.com>
2023-08-26 11:03:12 +0100

    version 1.0.0 for client side

---
+++ client/app/(auth)/login/complete/page.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import Logo from '@/components/MainPage/Logo/Logo';
+import './style.scss';
+
+export default function page() {
+  return (
+    <div className="complete-info">
+      <div className="container">
+        <div className="complete-box">
+          <h2>Set your Personal Details</h2>
+          <p>feel free to edit you basic information such as username, country...</p>
+          <div className="pic-box profile-pic-box">
+
+          </div>
+          <div className="pic-box cover-pic-box">
+
+          </div>
+          <div className="extra-info">
+
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}

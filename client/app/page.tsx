2f466f1284927be63736f685366b415c21ef1612
ZAKARIA RABHI <zac.rabhi12@gmail.com>
2023-08-26 11:18:45 +0100

    minor changes

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -2,7 +2,6 @@ import "@/public/images/logo.svg"
 import './hero-section.scss'
 import Navbar from './../components/MainPage/NavBar/Navbar'
 import MainContent from './../components/MainPage/MainContent/MainContent'
-import ScrollDown from '../components/MainPage/ScrollDown/ScrollDown'
 import { Routes, Route } from "react-router-dom";
 
 export default function Home() {

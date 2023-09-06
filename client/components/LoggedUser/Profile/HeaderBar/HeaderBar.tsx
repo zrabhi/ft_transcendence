63f0f622ea492f95e40f4fca139f57c815d881c0
Mohamed KARIM <karimkar999@gmail.com>
2023-09-06 09:50:27 +0100

    add the header component with search bar

--- client/components/LoggedUser/Profile/HeaderBar/HeaderBar.tsx
+++ client/components/LoggedUser/Profile/HeaderBar/HeaderBar.tsx
@@ -1,13 +1,11 @@
 import React from 'react'
 import SearchBar from '@/components/LoggedUser/Profile/HeaderBar/SearchBar/SearchBar'
-import Notifications from '@/components/LoggedUser/Profile/HeaderBar/Notifications/Notifications'
 import './HeaderBar.scss'
 
 export default function HeaderBar() {
   return (
     <div className="header-bar">
       <SearchBar />
-      <Notifications />
     </div>
   )
 }

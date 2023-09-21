b908504e58c8702694722773145ad63d699a20a6
Mohamed KARIM <karimkar999@gmail.com>
2023-09-21 10:30:28 +0100

    update the footer component

--- client/components/MainPage/Footer/Footer.tsx
+++ client/components/MainPage/Footer/Footer.tsx
@@ -9,6 +9,8 @@ export default function Footer() {
         <Link href="https://github.com/skeet1" target='_blank'>Skeet</Link>
         <Link href="https://github.com/zrabhi" target='_blank'> Zac</Link>
         <Link href="https://github.com/youssef-badaoui" target='_blank'> YOS3F</Link>
+        <Link href="https://github.com/mza7a" target='_blank'> Mza7a</Link>
+        <Link href="https://github.com/O" target='_blank'> OussamaBakhti</Link>
       </p>
     </div>
   )

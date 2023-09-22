294233464b45d7aae3a029790b60fe9923d4144a
Mohamed KARIM <karimkar999@gmail.com>
2023-09-22 18:44:23 +0100

    Modal PopUp can be disappear if click on close button or by click outside the boxModal

--- client/components/LoggedUser/Profile/PopUpModal/PopUpModal.tsx
+++ client/components/LoggedUser/Profile/PopUpModal/PopUpModal.tsx
@@ -6,11 +6,16 @@ export default function PopUpModal({ isVisible, close }: any) {
 
   if (!isVisible) return (<></>)
 
+  const handleCloseModal = (e: any) => {
+    if (e.target.classList.contains('modal')) close()
+  }
+
   return (
     <div className="modal fixed inset-0 bg-slate-400 bg-opacity-20 
       backdrop-blur-sm flex justify-center items-center
       z-50
       "
+      onClick={handleCloseModal}
     >
       <div className="content w-[40rem] xs:w-[18rem] sm:w-[24rem] md:w-[30rem]
         bg-slate-500 rounded-xl bg-opacity-50 relative p-4 pt-12 pb-8

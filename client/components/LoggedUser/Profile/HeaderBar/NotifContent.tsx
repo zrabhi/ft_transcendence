f4e5f46c9386b6f160b136241f02e04e2a06d518
Mohamed KARIM <karimkar999@gmail.com>
2023-09-29 18:04:03 +0100

    feat: render custom notifcation and create prototype of all notifaction type

--- client/components/LoggedUser/Profile/HeaderBar/NotifContent.tsx
+++ client/components/LoggedUser/Profile/HeaderBar/NotifContent.tsx
@@ -1,12 +1,53 @@
 import React from 'react'
+import NotifItem from './NotifItem'
 
 export default function NotifContent() {
+
+  // notif types 
+  //   1 => friendRequest
+  //   2 => recieveMessage
+  //   3 => gameRequest
+
+  // notif has avatar and username of the sender and the type of notif
+  // and we will discuss the notif how to check the stats of the notif
+  // friendRequest => accept or reject
+  // recieveMessage => onClick => redirect to chat
+  // gameRequest => accept or reject
+
+  const notif = [
+    {
+      type: 1,
+      avatar: 'https://i.pravatar.cc/300',
+      username: 'user1',
+    },
+    {
+      type: 2,
+      avatar: 'https://i.pravatar.cc/300',
+      username: 'user2',
+    },
+    {
+      type: 3,
+      avatar: 'https://i.pravatar.cc/300',
+      username: 'user3',
+    },
+    {
+      type: 4,
+      avatar: 'https://i.pravatar.cc/300',
+      username: 'user4',
+    }
+  ]
+
   return (
     <div className="notif-content">
-      <h3 className='text-lg tracking-wider font-semibold'>
+      <h3 className='text-lg tracking-wider font-semibold absolute top-6'>
         Notifications
       </h3>
-      <div className="notif-list">
+      <div className="notif-list mt-2">
+        {
+          notif.map((item, index) => (
+            <NotifItem key={index} data={item} />
+          ))
+        }
       </div>
     </div>
   )
---
+++ client/components/LoggedUser/Profile/HeaderBar/NotifMessage.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function NotifMessage() {
+  return (
+    <div>NotifMessage</div>
+  )
+}

6a702ecbbf275c3ba4e76ea22f7e094ca692efc0
Mohamed KARIM <karimkar999@gmail.com>
2023-09-30 14:16:30 +0100

    Add user card and can go to user profile from search

--- client/components/LoggedUser/Profile/UserProfile/UserCard/UserCard.tsx
+++ client/components/LoggedUser/Profile/UserProfile/UserCard/UserCard.tsx
@@ -1,35 +1,36 @@
 import React from 'react';
 
-import './ProfileCard.scss';
+import './UserCard.scss';
 
-export default function ProfileCard(user: any) {
+export default function UserCard(user: any) {
+  user = user.user;
   return (
     <div className="profile-card w-full relative ">
       <div className="background"></div>
       <div className="avatar">
         <img
           src={
-            user.data && user.data.avatar && !user.data.avatar.includes('googleusercontent')
-              ? user.data.avatar
+            user && user.avatar && !user.avatar.includes('googleusercontent')
+              ? user.avatar
               : '/images/DefaultAvatar.jpg'
           }
           alt="User Avatar"
         />
       </div>
       <div className="user-details">
-        <h2>{user.data && user.data.username}</h2>
+        <h2>{user && user.username}</h2>
         <div className="stats">
           <div className="stat-item">
             <h4>Total Games</h4>
-            <p>{user.data && user.data.totalGames}</p>
+            <p>{user && user.totalGames}</p>
           </div>
           <div className="stat-item">
             <h4>Wins</h4>
-            <p>{user.data && user.data.wins}</p>
+            <p>{user && user.wins}</p>
           </div>
           <div className="stat-item">
             <h4>Losses</h4>
-            <p>{user.data && user.data.losses}</p>
+            <p>{user && user.losses}</p>
           </div>
         </div>
       </div>
--- client/components/LoggedUser/Profile/UserProfile/UserProfile.tsx
+++ client/components/LoggedUser/Profile/UserProfile/UserProfile.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { useState, useEffect } from 'react';
 import { baseUrlUsers, getRequest } from '@/app/context/utils/service';
-import ProfileCard from '../ProfileCard/ProfileCard';
 import Image from 'next/image';
+import UserCard from './UserCard/UserCard';
 
 export default function UserProfile({ username }: { username: string }) {
   const [user, setUser] = useState<any>(null);
@@ -13,14 +13,11 @@ export default function UserProfile({ username }: { username: string }) {
       try {
         const allUsers = await getRequest(`${baseUrlUsers}/users`);
         const currentUser = allUsers.filter((user: any) => user.username === username );
-        console.log(currentUser.length);
+        // console.log(currentUser.length);
         if (currentUser.length === 0) return setUser(null)
         else {
-          // setUser(currentUser);
-          // // console.log(user.length)
-          console.log(currentUser[0].username);
+          // console.log(currentUser[0].username);
           setUser(currentUser[0]);
-          // console.log(user.username);
         }
         setLoading(false);
       } catch (error) {
@@ -43,14 +40,7 @@ export default function UserProfile({ username }: { username: string }) {
   if (!user) return <div>User not found</div>;
   return (
     <div className='text-slate-50'>
-      UserProfile
-      <h1>the user is : {user.username}</h1>
-      <Image
-        src={user.avatar}
-        alt="User Avatar"
-        width={100}
-        height={100}
-      />
+      <UserCard user={user} />
     </div>
   )
 }

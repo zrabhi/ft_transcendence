ce97fa59fd859a6a3c1f31c9cd2a65be241f489d
Mohamed KARIM <karimkar999@gmail.com>
2023-10-21 16:19:56 +0100

    Add footer to page again

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -39,7 +39,7 @@ export default function RootLayout({
         <AuthProvider>
           {children}
         </AuthProvider>
-        {/* <Footer /> */}
+        <Footer />
       </body>
     </html>
   );

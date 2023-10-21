e632560c776d9ce0b0be93212a6ace07b634b861
Mohamed KARIM <karimkar999@gmail.com>
2023-10-21 18:53:08 +0100

    add quotes for stats

--- client/components/LoggedUser/Profile/ProfileData/Stat.tsx
+++ client/components/LoggedUser/Profile/ProfileData/Stat.tsx
@@ -1,14 +1,15 @@
 // Stat.js
 import Image from 'next/image';
 
-export default function Stat({ icon, label, value }: any) {
+export default function Stat({ icon, label, value, desc }: { icon: any, label: string, value: number, desc: string }) {
   return (
     <div className="flex gap-8 p-8 items-center rounded-[.3rem] bg-purple-400 bg-opacity-10 hover:bg-opacity-40 transition-all duration-100">
       <div className="stat-img">
         <Image src={icon} alt={label} width={60} height={60} />
       </div>
       <div className="stat-text">
-        <p className="text-slate-100">{label}: {value}</p>
+        <p className="font-bold text-xl tracking-wider ">{label}: {value}</p>
+        <p className="opacity-60">{desc}</p>
       </div>
     </div>
   );
--- client/components/LoggedUser/Profile/ProfileData/StatsData.tsx
+++ client/components/LoggedUser/Profile/ProfileData/StatsData.tsx
@@ -10,10 +10,11 @@ export default function StatsData({ user }: any) {
   return (
     <div className="mx-auto p-4 mt-4 rounded-[.5rem] shadow-md">
       <div className="text-data flex flex-col gap-1">
-        <Stat icon={winImage} label="Wins" value={user.win} />
-        <Stat icon={lossImage} label="Losses" value={user.loss} />
-        <Stat icon={scoredImage} label="Goals Scored" value={user.totalGoalsScored} />
-        <Stat icon={receivedImage} label="Goals Received" value={user.totalGoalsRecieved} />
+        {/* add new prop as a quote for the curent state  */}
+        <Stat icon={winImage} label="Wins" value={user.win} desc="If you want it, GO FOR IT!" />
+        <Stat icon={lossImage} label="Losses" value={user.loss} desc="Every loss is a lesson." />
+        <Stat icon={scoredImage} label="Goals Scored" value={user.totalGoalsScored} desc="Scoring goals is like scoring in life." />
+        <Stat icon={receivedImage} label="Goals Received" value={user.totalGoalsRecieved} desc="Defending is just as important as attacking." />
       </div>
     </div>
   );
